4a72435d7a62c07b1f030b0dd661b781fed3a6b3
Safurudin Mahic <safurudin.mahic@vegvesen.no>
2020-02-13 10:10:11 +0100

    Added bintray config to build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,27 @@
+val artifactName = "datex-client"
+val artifactGroup = "no.vegvesen"
+val artifactVersion = "1.2.0"
+
+val pomUrl = "https://github.com/vegvesen/datex-client"
+val pomScmUrl = "https://github.com/vegvesen/datex-client"
+val pomScmConnection = "scm:git:git://github.com/vegvesen/datex-client.git"
+val pomIssueUrl = "https://github.com/vegvesen/datex-client/issues"
+val pomDesc = "A serializer which can deserialize Datex II publications from XML to JAXB POJOs."
+
+val githubRepo = "vegvesen/datex-client"
+val githubReadme = "README.md"
+
+val pomLicenseName = "MIT License"
+val pomLicenseUrl = "https://opensource.org/licenses/mit-license.php"
+val pomLicenseDist = "repo"
+
+val pomDeveloperId = "mahic"
+val pomDeveloperName = "Safurudin Mahic"
+
 plugins {
     java
     `maven-publish`
+    id("com.jfrog.bintray") version "1.8.4"
 }
 
 java {
@@ -10,6 +31,7 @@ java {
 
 repositories {
     mavenCentral()
+    jcenter()
 }
 
 dependencies {
@@ -21,7 +43,7 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
-    maxHeapSize = "64m"
+    maxHeapSize = "1024m"
 }
 
 tasks.register<Jar>("sourcesJar") {
@@ -32,12 +54,59 @@ tasks.register<Jar>("sourcesJar") {
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            groupId = "no.vegvesen"
-            artifactId = "datex-client"
-            version = "1.0"
-
+            groupId = artifactGroup
+            artifactId = artifactId
+            version = artifactVersion
             from(components["java"])
             artifact(tasks["sourcesJar"])
+
+            pom.withXml {
+                asNode().apply {
+                    appendNode("description", pomDesc)
+                    appendNode("name", rootProject.name)
+                    appendNode("url", pomUrl)
+                    appendNode("licenses").appendNode("license").apply {
+                        appendNode("name", pomLicenseName)
+                        appendNode("url", pomLicenseUrl)
+                        appendNode("distribution", pomLicenseDist)
+                    }
+                    appendNode("developers").appendNode("developer").apply {
+                        appendNode("id", pomDeveloperId)
+                        appendNode("name", pomDeveloperName)
+                    }
+                    appendNode("scm").apply {
+                        appendNode("url", pomScmUrl)
+                        appendNode("connection", pomScmConnection)
+                    }
+                }
+            }
+        }
+    }
+}
+
+bintray {
+    user = project.findProperty("bintrayUser").toString()
+    key = project.findProperty("bintrayKey").toString()
+    publish = true
+    setPublications("maven")
+
+    pkg.apply {
+        repo = "maven"
+        name = artifactName
+        userOrg = "vegvesen"
+        githubRepo = githubRepo
+        vcsUrl = pomScmUrl
+        description = pomDesc
+        setLicenses("MIT")
+        desc = description
+        websiteUrl = "https://vegvesen.no"
+        issueTrackerUrl = pomIssueUrl
+        githubReleaseNotesFile = githubReadme
+
+        version.apply {
+            name = artifactVersion
+            desc = pomDesc
+            vcsTag = artifactVersion
         }
     }
 }
